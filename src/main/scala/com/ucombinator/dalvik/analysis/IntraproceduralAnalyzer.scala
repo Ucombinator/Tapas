85496f09e4bfaf236ba9c230574c0d1e88653635
Petey <petey.aldous@utah.edu>
2013-06-06 10:17:19 -0600

    intraprocedural analyzer - it compiles but doesn't do anything

---
+++ src/main/scala/com/ucombinator/dalvik/analysis/IntraproceduralAnalyzer.scala
@@ -0,0 +1,14 @@
+package com.ucombinator.dalvik.analysis
+
+import com.ucombinator.dalvik.AST._
+
+class IntraproceduralAnalyzer(classes: Array[ClassDef]) extends SimpleMethodCallGraph(classes: Array[ClassDef]) {
+  override def processInstructions(methodProxy: MethodDefProxy)(code: CodeItem) {
+    val add = addMethod(methodProxy)_
+    code.insns foreach {
+      (insn) => insn match {
+        case _ => false
+      }
+    }
+  }
+}
