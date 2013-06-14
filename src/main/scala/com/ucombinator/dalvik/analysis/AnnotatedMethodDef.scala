383736bc7fd0a07b226fa3068a6059584aef4776
Petey <petey.aldous@utah.edu>
2013-06-14 15:51:34 -0600

    another baby step

--- src/main/scala/com/ucombinator/dalvik/analysis/AnnotatedMethodDef.scala
+++ src/main/scala/com/ucombinator/dalvik/analysis/AnnotatedMethodDef.scala
@@ -6,15 +6,17 @@ import com.ucombinator.dalvik.AST.Method
 import com.ucombinator.dalvik.AST.Instruction
 
 class AnnotatedMethodDef(method: Method, accessFlags: Long, code: CodeItem) extends MethodDef(method, accessFlags, code) {
-	// Apparently goto statements take Ints, so that's what we'll use
+	// Apparently goto statements take Int objects as targets, so that's what we'll use
 	val labelTable: Map[Int, List[Instruction]] = Map.empty
-	// TODO: Think about how to represent inputs
-	private val inputs: Set[Int] = Set.empty
-	// TODO: outputs probably get the same representation
-	private val outputs: Set[Int] = Set.empty
+	// The idea behind using Address objects for inputs and outputs is to avoid aliasing problems
+	private val inputs: Set[Address] = Set.empty
+	private val outputs: Set[Address] = Set.empty
 	
-	// TODO: define this function. Also, the Int objects should probably not be Ints.
-	def influence(input: Int): Set[Int] = Set.empty
+	private val influences: Set[(Address, Address)] = Set.empty
+	
+	// TODO there's probably a more colloquial way to write these functions
+	def affectedSinks(input: Address): Set[Address] = influences.filter(pair => pair._1 == input).map(pair => pair._2)
+	def sources(output: Address): Set[Address] = influences.filter(pair => pair._2 == output).map(pair => pair._1)
 	
 	// pseudo-constructor. Is there a better way to write this?
 	def annotate(method: MethodDef): AnnotatedMethodDef = new AnnotatedMethodDef(method.method, method.accessFlags, method.code)
