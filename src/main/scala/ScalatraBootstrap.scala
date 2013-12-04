1fa323d5afa23d684a228e31833b2a18e9186e24
Andy Keep <akeep@robotman.org>
2013-12-03 21:16:31 -0700

    added the start of a web app.  also, using a slightly different version of sbt (included in the home directory) to support some of the features required by Scalatra, the web micro-framework that I'm using.

---
+++ src/main/scala/ScalatraBootstrap.scala
@@ -0,0 +1,21 @@
+import com.ucombinator.webapp._
+import org.scalatra._
+import javax.servlet.ServletContext
+import com.mchange.v2.c3p0.ComboPooledDataSource
+import slick.session.Database
+
+class ScalatraBootstrap extends LifeCycle {
+  val cpds = new ComboPooledDataSource
+
+  override def init(context: ServletContext) = {
+    val db = Database.forDataSource(cpds)
+    context.mount(new TapasServlet(db), "/*")
+  }
+
+  private def closeDbConnection() = cpds.close
+
+  override def destroy(context: ServletContext) = {
+    super.destroy(context)
+    closeDbConnection
+  }
+}
---
+++ src/main/scala/com/ucombinator/webapp/TapasServlet.scala
@@ -0,0 +1,19 @@
+package com.ucombinator.webapp
+
+import org.scalatra._
+import scalate.ScalateSupport
+import slick.session.Database
+
+class TapasServlet(val db: Database) extends TapasWebAppStack {
+  get("/") {
+    <html>
+      <head>
+        <title>Tapas Web App</title>
+      </head>
+      <body>
+        <h1>Welcome to the Tapas Web App!</h1>
+        <p>Eventually there will be an application here.</p>
+      </body>
+    </html>
+  }
+}
